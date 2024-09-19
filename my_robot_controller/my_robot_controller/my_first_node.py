1eadfad7de68a95a225d24d465c6b8f63582ff0e
Benjamin Saenz <bsaenz454@gmail.com>
2024-09-19 10:14:43 +0000

    creatted subscriber

--- my_robot_controller/my_robot_controller/my_first_node.py
+++ my_robot_controller/my_robot_controller/my_first_node.py
@@ -1,12 +1,17 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-
 
 class MyNode(Node):
 
     def __init__(self):
         super().__init__("first_node")
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello " + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     # initialize ros2 communcations
@@ -15,6 +20,10 @@ def main(args=None):
     # nodes go here
     node = MyNode()
 
+    # node is kept alive until the user intervenes
+    rclpy.spin(node)
+
+    # shutdown all nodes and communications
     rclpy.shutdown()
 
 if __name__ == '__main__':
